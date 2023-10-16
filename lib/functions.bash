f4b6deedd5d46eb289d723549e322ff3aa7e7d5a
Francis M <francis@destinatech.com>
2023-10-16 14:25:00 +0000

    lib/functions.bash: Remove unused opt_arg()

--- lib/functions.bash
+++ lib/functions.bash
@@ -14,22 +14,6 @@ function die()
   exit 1
 }
 
-## {{{ function opt_arg()
-function opt_arg()
-{
-  [[ $# -lt 2 ]] && die "function opt_arg() requires at least 3 args (got $#)"
-
-  local opt="$1"; shift
-  local arg="$1"; shift
-
-  # --foo=bar
-  arg="${arg#$opt=}"
-  echo -n "$arg"
-
-  return 0
-}
-## }}}
-
 ##
 # vim: ts=2 sw=2 et fdm=marker :
 ##
